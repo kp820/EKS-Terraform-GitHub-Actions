4ea030b12224c6efe56283fd1219cf82913de684
Kamta Prajapati <67824988+kp820@users.noreply.github.com>
2024-08-17 11:54:05 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 1.9.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "~> 5.49.0"  
     }
   }
   backend "s3" {
